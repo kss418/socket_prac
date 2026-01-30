676ae0132e00b163eb3cf916f4d0582c1446d367
kss418 <jis1595@naver.com>
2026-01-31 04:49:59 +0900

    헤더 추가

--- apps/server.cpp
+++ apps/server.cpp
@@ -4,34 +4,7 @@
 #include "../include/unique_fd.hpp"
 #include "../include/addr.hpp"
 #include "../include/fd_helper.hpp"
-
-constexpr int BUF_SIZE = 4096;
-
-std::expected <void, int> echo_session(int client_fd){
-    std::array <char, BUF_SIZE> buf{};
-    while(true){
-        ssize_t recv_byte = ::recv(client_fd, buf.data(), buf.size(), 0);
-        if(recv_byte == 0) return {};
-        if(recv_byte == -1){
-            int ec = errno;
-            if(errno == EINTR) continue;
-            return std::unexpected(ec);
-        }
-
-        ssize_t send_byte = 0;
-        while(send_byte < recv_byte){
-            ssize_t now = ::send(client_fd, buf.data() + send_byte, recv_byte - send_byte, MSG_NOSIGNAL);
-            if(now == -1){
-                int ec = errno;
-                if(ec == EINTR) continue;
-                return std::unexpected(ec);
-            }
-
-            if(now == 0) return std::unexpected(EPIPE);
-            send_byte += now;
-        }
-    }
-}
+#include "../include/session.hpp"
 
 int main(){
     auto addr_exp = get_addr_server("8080");
