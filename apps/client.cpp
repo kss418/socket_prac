6df5c9b0b68d484696966764c96c441381b6543d
kss418 <jis1595@naver.com>
2026-02-02 20:39:27 +0900

    chat_client class 추가

--- apps/client.cpp
+++ apps/client.cpp
@@ -1,30 +1,16 @@
-#include <iostream>
-#include "../include/unique_fd.hpp"
-#include "../include/addr.hpp"
-#include "../include/fd_helper.hpp"
-#include "../include/session.hpp"
-#include "../include/error_code.hpp"
-#include "../include/io_helper.hpp"
+#include "../include/chat_client.hpp"
 
 int main(){
-    auto addr_exp = get_addr_client("127.0.0.1", "8080");
-    if(!addr_exp){
-        handle_error("get_addr_client failed", addr_exp);
+    chat_client client;
+    auto conn_exp = client.connect("127.0.0.1", "8080");
+    if(!conn_exp){
+        handle_error("client connect failed", conn_exp);
         return 1;
     }
 
-    auto server_fd_exp = make_server_fd(addr_exp->get());
-    if(!server_fd_exp){
-        handle_error("make_server_fd failed", server_fd_exp);
-        return 1;
-    }
-
-    unique_fd server_fd = std::move(*server_fd_exp);
-    socket_info si{};
-
-    auto session_exp = echo_client(server_fd.get(), si);
-    if(!session_exp){
-        handle_error("echo_client failed", session_exp);
+    auto run_exp = client.run();
+    if(!run_exp){
+        handle_error("client run failed", run_exp);
         return 1;
     }
 
---
+++ src/chat_client.cpp
@@ -0,0 +1,44 @@
+#include "../include/chat_client.hpp"
+#include "../include/addr.hpp"
+
+std::expected <void, error_code> chat_client::connect(const char* ip, const char* port){
+    auto addr_exp = get_addr_client(ip, port);
+    if(!addr_exp){
+        handle_error("get_addr_client failed", addr_exp);
+        return std::unexpected(addr_exp.error());
+    }
+
+    auto server_fd_exp = make_server_fd(addr_exp->get());
+    if(!server_fd_exp){
+        handle_error("make_server_fd failed", server_fd_exp);
+        return std::unexpected(server_fd_exp.error());
+    }
+
+    server_fd = std::move(*server_fd_exp);
+    si = {};
+    return {};
+}
+
+std::expected <void, error_code> chat_client::run(){
+    while(true){
+        std::string s;
+        if(!std::getline(std::cin, s)) return {};
+
+        s.push_back('\n');
+        si.append(s);
+        auto flush_send_exp = flush_send(server_fd.get(), si);
+        if(!flush_send_exp){
+            handle_error("flush_send failed", flush_send_exp);
+            continue;
+        }
+
+        auto recv_ret_exp = drain_recv(server_fd.get(), si);
+        if(!recv_ret_exp){
+            handle_error("drain_recv failed", recv_ret_exp);
+            return std::unexpected(recv_ret_exp.error());
+        }
+
+        flush_recv(si.recv_buf);
+        if(recv_ret_exp->closed) return {};
+    }
+}
