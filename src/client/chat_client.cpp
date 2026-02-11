1e7cc1331dc7b90439ca63d920dff5fb3837a294
kss418 <jis1595@naver.com>
2026-02-11 20:02:49 +0900

    클라이언드 io, work 서로 다른 스레드에서 작동하게 변경

--- src/client/chat_client.cpp
+++ src/client/chat_client.cpp
@@ -1,6 +1,13 @@
 #include "client/chat_client.hpp"
 #include "net/addr.hpp"
 #include "protocol/line_parser.hpp"
+#include <poll.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <array>
+#include <condition_variable>
+#include <mutex>
+#include <optional>
 
 std::expected <void, error_code> chat_client::connect(const char* ip, const char* port){
     auto addr_exp = get_addr_client(ip, port);
@@ -21,39 +28,128 @@ std::expected <void, error_code> chat_client::connect(const char* ip, const char
 }
 
 std::expected <void, error_code> chat_client::run(){
-    while(true){
-        std::string s;
-        if(!std::getline(std::cin, s)) return {};
-        if(s.empty()) continue;
+    std::mutex state_mtx;
+    std::condition_variable state_cv;
+    std::optional<error_code> error_opt;
+    bool stop_requested = false;
 
-        si.send.append(command_codec::cmd_say{s});
-        auto flush_send_exp = flush_send(server_fd.get(), si);
-        if(!flush_send_exp){
-            handle_error("flush_send failed", flush_send_exp);
-            continue;
+    auto signal_stop = [&](std::optional<error_code> ec = std::nullopt){
+        {
+            std::lock_guard<std::mutex> lock(state_mtx);
+            if(ec && !error_opt) error_opt = *ec;
+            stop_requested = true;
         }
+        state_cv.notify_one();
+    };
 
-        auto recv_ret_exp = drain_recv(server_fd.get(), si);
-        if(!recv_ret_exp){
-            handle_error("drain_recv failed", recv_ret_exp);
-            return std::unexpected(recv_ret_exp.error());
+    std::jthread execute_thread([this, &signal_stop](std::stop_token st){
+        execute_loop(st);
+        signal_stop();
+    });
+
+    std::jthread io_thread([this, &signal_stop](std::stop_token st){
+        auto io_exp = io_loop(st);
+        if(!io_exp){
+            handle_error("io_loop error", io_exp);
+            signal_stop(io_exp.error());
         }
+        else signal_stop();
+    });
 
-        while(true){
-            auto line = line_parser::parse_line(si.recv.raw());
-            if(!line) break;
+    {
+        std::unique_lock<std::mutex> lock(state_mtx);
+        state_cv.wait(lock, [&](){ return stop_requested; });
+    }
 
-            auto dec_exp = command_codec::decode(*line);
-            if(!dec_exp){
-                handle_error("decode failed", dec_exp);
-                continue;
-            }
+    execute_thread.request_stop();
+    execute_cv.notify_one();
 
-            auto cmd = std::move(*dec_exp);
+    io_thread.request_stop();
+    ::shutdown(server_fd.get(), SHUT_RDWR);
+
+    execute_thread.join();
+    io_thread.join();
+
+    if(error_opt) return std::unexpected(*error_opt);
+    return {};
+}
+
+void chat_client::request_execute(const command_codec::command& cmd){
+    {
+        std::lock_guard<std::mutex> lock(execute_mtx);
+        execute_q.emplace(cmd);
+    }
+
+    execute_cv.notify_one();
+}
+
+void chat_client::execute_loop(std::stop_token stop_token){
+    while(!stop_token.stop_requested()){
+        std::queue <command_codec::command> pending_q;
+        {
+            std::unique_lock<std::mutex> lock(execute_mtx);
+            execute_cv.wait(lock, [&](){ return !execute_q.empty() || stop_token.stop_requested(); });
+            std::swap(pending_q, execute_q);
+        }
+        
+        while(!pending_q.empty()){
+            auto cmd = std::move(pending_q.front());
+            pending_q.pop();
             execute(cmd);
         }
+    }
+}
 
-        if(recv_ret_exp->closed) return {};
+std::expected <void, error_code> chat_client::io_loop(std::stop_token stop_token){
+    std::array <pollfd, 2> fds{{
+        { .fd = STDIN_FILENO, .events = POLLIN, .revents = 0 },
+        { .fd = server_fd.get(), .events = POLLIN | POLLHUP | POLLERR, .revents = 0 }
+    }};
+
+    while(!stop_token.stop_requested()){
+        int size = ::poll(fds.data(), fds.size(), -1);
+        if(size < 0){
+            int ec = errno;
+            if(errno == EINTR) continue;
+            return std::unexpected(error_code::from_errno(ec));
+        }
+
+        if(fds[0].revents & (POLLIN | POLLHUP)){
+            auto send_exp = send();
+            if(!send_exp) return std::unexpected(send_exp.error());
+            if(*send_exp) break;
+        }
+
+        if(fds[1].revents & POLLERR){
+            int so_error = 0;
+            socklen_t len = sizeof(so_error);
+            if(::getsockopt(server_fd.get(), SOL_SOCKET, SO_ERROR, &so_error, &len) == -1){
+                return std::unexpected(error_code::from_errno(errno));
+            }
+
+            if(so_error == 0) so_error = EIO;
+            return std::unexpected(error_code::from_errno(so_error));
+        }
+        
+        if(fds[1].revents & (POLLIN | POLLHUP)){
+            auto recv_exp = recv();
+            if(!recv_exp) return std::unexpected(recv_exp.error());
+
+            while(auto line = line_parser::parse_line(si.recv.raw())){
+                auto dec_exp = command_codec::decode(*line);
+                if(!dec_exp){
+                    handle_error("execute_loop/decode failed", dec_exp);
+                    continue;
+                }
+
+                request_execute(*dec_exp);
+            }
+            
+            if(*recv_exp) break;
+        }
+
+        fds[0].revents = 0;
+        fds[1].revents = 0;
     }
 
     return {};
@@ -74,4 +170,32 @@ void chat_client::execute(const command_codec::command& cmd){
             std::cout << c.text << "\n"; 
         }
     }, cmd);
+}
+
+std::expected <bool, error_code> chat_client::send(){
+    char tmp[1024];
+    ssize_t recv_byte = ::read(STDIN_FILENO, tmp, sizeof(tmp));
+    if(recv_byte == 0) return true;
+
+    if(recv_byte < 0){
+        int ec = errno;
+        if(errno == EINTR) return false;
+        return std::unexpected(error_code::from_errno(ec));
+    }
+
+    buf.append(tmp, static_cast<size_t>(recv_byte));
+    while(auto line = line_parser::parse_line(buf)){
+        if(line->empty()) continue;
+        si.send.append(command_codec::cmd_say{*line});
+        auto fs_exp = flush_send(server_fd.get(), si);
+        if(!fs_exp) return std::unexpected(fs_exp.error());
+    }
+
+    return false;
+}
+
+std::expected <bool, error_code> chat_client::recv(){
+    auto dr_exp = drain_recv(server_fd.get(), si);
+    if(!dr_exp) return std::unexpected(dr_exp.error());
+    return dr_exp->closed;
 }
--- src/protocol/line_parser.cpp
+++ src/protocol/line_parser.cpp
@@ -4,11 +4,11 @@ bool line_parser::has_line(std::string_view recv_buf){
     return recv_buf.find('\n') != std::string_view::npos;
 }
 
-std::optional<std::string> line_parser::parse_line(std::string& recv_buf){
-    std::size_t pos = recv_buf.find('\n');
+std::optional<std::string> line_parser::parse_line(std::string& buf){
+    std::size_t pos = buf.find('\n');
     if(pos == std::string::npos) return std::nullopt;
 
-    std::string line = recv_buf.substr(0, pos);
-    recv_buf.erase(0, pos + 1);
+    std::string line = buf.substr(0, pos);
+    buf.erase(0, pos + 1);
     return line;
 }
